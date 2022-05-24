bf233616581a0a5246c9b261d6178a517e50d967
Mandy_Mascetti <armascetti@gmail.com>
2022-05-24 16:05:27 -0400

    valid parentheses correct

--- ValidParentheses.js
+++ ValidParentheses.js
@@ -1,19 +1,19 @@
-var isValid = function(s) {
-  let parenthesis = {
-      ")" : "(",
-      "}" : "{",
-      "]" : "[",
+var isValid = function (s) {
+  let pairs = {
+    ")": "(",
+    "}": "{",
+    "]": "[",
   }
   let arr = []
-  for (let i = 0; i < s.length; i++){
-      if(s[i] === "(" || s[i] === "[" || s[i] === "{"){
-          arr.push(s[i])
-      } else {
-          if(arr[arr.length -1] === parenthesis[s[i]]){
-              arr.pop()
-          }
-          else return false
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(" || s[i] === "[" || s[i] === "{") {
+      arr.push(s[i])
+    } else {
+      if (arr[arr.length - 1] === pairs[s[i]]) {
+        arr.pop()
       }
+      else return false
+    }
   }
-  return arr.length === 0 ? true : false 
+  return arr.length === 0 ? true : false
 };
