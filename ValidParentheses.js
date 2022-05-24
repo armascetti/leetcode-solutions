717ecd8ee90a508752b898ea6438eb100b790311
Mandy_Mascetti <armascetti@gmail.com>
2022-05-24 15:30:32 -0400

    valid parentheses

--- ValidParentheses.js
+++ ValidParentheses.js
@@ -1,7 +1,19 @@
-var isValid = function (s) {
-  if (s === "()" || "[]" || "{}") {
-    return true
-  } else {
-    return false
+var isValid = function(s) {
+  let parenthesis = {
+      ")" : "(",
+      "}" : "{",
+      "]" : "[",
   }
+  let arr = []
+  for (let i = 0; i < s.length; i++){
+      if(s[i] === "(" || s[i] === "[" || s[i] === "{"){
+          arr.push(s[i])
+      } else {
+          if(arr[arr.length -1] === parenthesis[s[i]]){
+              arr.pop()
+          }
+          else return false
+      }
+  }
+  return arr.length === 0 ? true : false 
 };
