f6580049d6a2f2cd95ae4d47368b75d00aa6b7b2
Mandy Mascetti <armascetti@gmail.com>
2026-06-14 19:16:57 -0400

    flattenarray

---
+++ FlattenArray.js
@@ -0,0 +1,32 @@
+function flatten(arr) {
+    let flat = [];
+    for (let i = 0; i < arr.length; i++) {
+        let item = arr[i];
+        if (!Array.isArray(item)) {
+            flat.push(item)
+        } else {
+           flat.push(...flatten(item))
+        }
+    }
+    return flat;
+}
+
+
+
+console.log(flatten([[1], [2], [3]]));
+// [1,2,3]
+
+// Create an empty result array
+
+// Loop through each item in arr
+
+//     IF item is NOT an array
+//         add item to result
+
+//     ELSE
+//         flatten the nested array
+
+//         add all flattened values
+//         into result
+
+// Return result
