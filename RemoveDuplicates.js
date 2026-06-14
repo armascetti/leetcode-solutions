4eb90016e44bbad608de1d433edb9eb9ca5d3c84
Mandy Mascetti <armascetti@gmail.com>
2026-06-14 13:01:00 -0400

    removeduplicates

---
+++ RemoveDuplicates.js
@@ -0,0 +1,14 @@
+function removeDuplicates(arr) {
+  // return unique values
+  let remove = new Set (arr)
+  let newArr = [...remove]
+  return newArr 
+ }
+
+
+console.log((removeDuplicates([1,2,2,3,4,4])));
+
+// [1,2,3,4]
+
+//create a new set 
+//push the array into the new set 
